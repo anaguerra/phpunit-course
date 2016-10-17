34260a440ff25fe76dfed915d1b3de2e5ca5297c
diegoseso <diegoseso@debian.linuxhome.com>
2016-10-17 23:27:55 +0200

    Basic solutions to the Class 1 Questions

---
+++ Class1/Repairment.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Class1;
+
+Class Repairment {
+
+    public function pullOutTyre( Car $car )
+    {
+   
+    }
+}
--- Tests/basicTest.php
+++ Tests/basicTest.php
@@ -3,6 +3,9 @@
 namespace Class1\Tests;
 
 use Class1\Person;
+use Class1\Repairment;
+use Class1\Car;
+
 
 Class class1Test extends \PHPUnit_Framework_TestCase
 {
@@ -20,4 +23,57 @@ Class class1Test extends \PHPUnit_Framework_TestCase
         $this->assertTrue( false );
     }
 
+    /**
+     *  
+     */
+    public function testRepairmentClassExists()
+    {
+        $repairment = new Repairment();
+        if ( $repairment instanceof Repairment ){
+            return $this->assertTrue( true );
+          
+        }
+        $this->assertTrue( false );
+    }
+
+    /**
+     *  
+     */
+    public function testCarClassExists()
+    {
+        $car = new Car();
+        if ( $car instanceof Car ){
+            return $this->assertTrue( true );
+          
+        }
+        $this->assertTrue( false );
+    }
+
+    /**
+     * @depends testRepairmentClassExists 
+     */
+    public function testMethodPullOutTyreExistsOnRepairment()
+    {
+        $repairment = new Repairment();
+        if ( in_array( 'pullOutTyre', get_class_methods( $repairment ) ) ){
+            return $this->assertTrue( true );
+        }
+        $this->assertTrue( false );        
+    }
+
+    /**
+     * @depends testMethodPullOutTyreExistsOnRepairment
+     */
+    public function testPullOutTyreTakesCarsOnly()
+    {
+        $reflection = new \ReflectionClass( 'Class1\Repairment' );
+        $reflectionMethod = $reflection->getMethod( 'pullOutTyre' );
+        $parameters = $reflectionMethod->getParameters();
+        
+        if( $parameters[0]->getClass()->name === 'Class1\Car' ){
+            return $this->assertTrue( true );
+        }
+        $this->assertTrue( false );
+       
+    } 
 } 
